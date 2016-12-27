6d766ede810186157a74c6a3a93cbbe2050bc350
Kristina McElveen <kmcelveen.sp26@gmail.com>
2016-12-26 23:55:08 -0800

    created another branch for extra credit and now working on modal, dynamic date and url. finally pushing up the basic requirements

--- client/app.js
+++ client/app.js
@@ -5,9 +5,8 @@
   let GameCarousel = function () {
         this.arrayOfGames = '';
         this.activeGame = 0;
-  }
+  };
   window['GameCarousel'] = GameCarousel;
-
 
     GameCarousel.prototype.loadToPage = function () {
       this.getAllData();
@@ -22,29 +21,30 @@
         dataType: 'json',
         cache: false
       }).done(function(data) {
-        console.log(data);
         that.arrayOfGames = data.data.games.game.slice();
         that.createThumbnails();
-        // console.log(that.games)
       }).fail(function(error){
           console.log(error);
       });
     };
-
     GameCarousel.prototype.createThumbnails = function (){
-      var template = '';
-      var that = this;
+      let template = '';
+      let that = this;
       try {
         $.each(that.arrayOfGames, function(i, game){
-            template = '<div class="game-thumbnail">';
-            template += '<div>'
+            template = '<div class="pill">';
+            template += '<h6>'+game.away_team_name+ ' @ ' + game.home_team_name+'</h6>';
+            template +='<div class="game-thumbnail">';
+            template += '<div>';
             template += '<img class="game-img" src ="' + game.video_thumbnails.thumbnail[0].content + '" >';
             template += '</div>';
             template += '</div>';
+            template += '<h6>'+ game.venue +'</h6>';
+            template += '<h6>'+ game.away_time + ' ' + game.away_ampm + ' ' + game.away_time_zone +'</h6>';
+            template +='</div>';
           $('#games-section').append(template);
         });
-        var gameSect = $('#games-section').children(":first");
-        console.log(gameSect)
+        let gameSect = $('#games-section').children(":first");
         gameSect.addClass('active');
       }
       catch(e) {
@@ -55,19 +55,17 @@
     GameCarousel.prototype.handleArrowControls = function (e){
       e.preventDefault();
       if(e.keyCode === 37 && this.activeGame != 0){
-          console.log('left arrow');
-            var test = $('.active').prev()
+            let activePill = $('.active').prev()
             $('.active').removeClass('active');
             this.activeGame--;
             window.scrollBy(-100, 0);
-            test.addClass('active');
+            activePill.addClass('active');
       }else if(e.keyCode === 39 && this.activeGame < this.arrayOfGames.length - 1){
-        console.log('right arrow');
-            var test = $('.active').next()
+            let activePill = $('.active').next()
             $('.active').removeClass('active');
             this.activeGame++;
             window.scrollBy(100, 0);
-            test.addClass('active');
+            activePill.addClass('active');
       }
     };
 } ( jQuery ) );
