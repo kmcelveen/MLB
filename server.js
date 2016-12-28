261f10f5d8b15793accf886e7d1b158a42499eb2
Kristina McElveen <kmcelveen.sp26@gmail.com>
2016-12-27 21:13:23 -0800

    added middleware to force https via heroku

--- server.js
+++ server.js
@@ -17,6 +17,19 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
+var https_redirect = function(req, res, next) {
+    if (process.env.NODE_ENV === 'production') {
+        if (req.headers['x-forwarded-proto'] != 'https') {
+            return res.redirect('https://' + req.headers.host + req.url);
+        } else {
+            return next();
+        }
+    } else {
+        return next();
+    }
+};
+
+app.use(https_redirect);
 /** Custom Setup for parsing the body of the request **/
 
 // app.use(function (req, res) {
