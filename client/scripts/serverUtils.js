582fa71da988c094b332aa2dc7a73b45b118b251
Kristina McElveen <kmcelveen.sp26@gmail.com>
2017-01-03 23:55:44 -0800

    clean up and refactor

--- client/scripts/serverUtils.js
+++ client/scripts/serverUtils.js
@@ -3,22 +3,20 @@
 module.export = {
 
   logErrors: (err, req, res, next) => {
-    console.error(err.stack)
-    next(err)
+    console.error(err.stack);
+    next(err);
   },
 
   clientSideErrorHandler: (err, req, res, next) => {
     if (req.xhr) {
-      res.status(500).send({ error: 'Something failed on the client!' })
+      res.status(500).send({ error: 'Something failed on the client!' });
     } else {
-      next(err)
+      next(err);
     }
   },
 
   errorHandler: (err, req, res, next) => {
     res.status(500)
-    res.render('error', { error: err })
+    res.render('error', { error: err });
   }
-
-
-}
+};
--- server.js
+++ server.js
@@ -1,55 +1,36 @@
 var express = require('express'),
+    app = express(),
     bodyParser = require('body-parser'),
     path = require('path'),
-    cors = require('cors')
+    cors = require('cors'),
     utils = require('./client/scripts/serverUtils.js'),
-    app = express(),
     PORT = process.env.PORT || 3000;
 
+function httpsRedirect (req, res, next) {
+  if (process.env.NODE_ENV === 'production') {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect('https://' + req.headers.host + req.url);
+    } else {
+      return next();
+    }
+  } else {
+    return next();
+  }
+}
 
 /** Middleware **/
 
 app.use(express.static(path.join(__dirname, 'client')));
-
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(bodyParser.json());
-
 app.use(cors());
-
-var https_redirect = function(req, res, next) {
-    if (process.env.NODE_ENV === 'production') {
-        if (req.headers['x-forwarded-proto'] != 'https') {
-            return res.redirect('https://' + req.headers.host + req.url);
-        } else {
-            return next();
-        }
-    } else {
-        return next();
-    }
-};
-
-app.use(https_redirect);
-/** Custom Setup for parsing the body of the request **/
-
-// app.use(function (req, res) {
-//   res.setHeader('Content-Type', 'text/plain')
-//   res.write('you posted:\n')
-//   res.end(JSON.stringify(req.body, null, 2))
-// });
-
-/** Error handling **/
-
-// app.use(methodOverride());
-// app.use(utils.logErrors);
-// app.use(utils.clientSideErrorHandler);
-// app.use(utils.errorHandler);
+app.use(httpsRedirect);
 
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
   console.log('Listening on port '.concat(PORT));
 });
