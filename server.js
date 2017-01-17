15360b154c5e3dcc431242d4f4fc98588bdba5eb
Kristina McElveen <kmcelveen.sp26@gmail.com>
2017-01-17 10:00:12 -0800

    refactored and add more features for user experience

--- server.js
+++ server.js
@@ -6,25 +6,12 @@ var express = require('express'),
   utils = require('./client/scripts/serverUtils.js'),
   PORT = process.env.PORT || 3000;
 
-var httpsRedirect = (req, res, next) => {
-  if (process.env.NODE_ENV === 'production') {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect('https://' + req.headers.host + req.url);
-    } else {
-      return next();
-    }
-  } else {
-    return next();
-  }
-};
-
 /** Middleware **/
 
 app.use(express.static(path.join(__dirname, 'client')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use(httpsRedirect);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
